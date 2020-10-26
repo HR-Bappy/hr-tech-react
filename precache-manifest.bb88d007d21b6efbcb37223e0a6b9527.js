96a71b87e33fefa4a9b407ca8e232186b0b7e0e4
HR Bappy <mdhabiburrb@gmail.com>
2020-10-27 01:19:25 +0600

    Updates

---
+++ precache-manifest.bb88d007d21b6efbcb37223e0a6b9527.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d76b949d010e34cf8af87693ed899616",
+    "url": "/hr-tech-react/index.html"
+  },
+  {
+    "revision": "af14339b1e64d38dfd27",
+    "url": "/hr-tech-react/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "63bcc9189ba947275810",
+    "url": "/hr-tech-react/static/css/main.80b6f01e.chunk.css"
+  },
+  {
+    "revision": "af14339b1e64d38dfd27",
+    "url": "/hr-tech-react/static/js/2.3736f935.chunk.js"
+  },
+  {
+    "revision": "e53cf286e00aab3498ae2b2d3185567a",
+    "url": "/hr-tech-react/static/js/2.3736f935.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63bcc9189ba947275810",
+    "url": "/hr-tech-react/static/js/main.2ce78857.chunk.js"
+  },
+  {
+    "revision": "4c69bb8eeb69fa400e08",
+    "url": "/hr-tech-react/static/js/runtime-main.96011ca6.js"
+  },
+  {
+    "revision": "50beeabb106b9e1666a0ff03468d68c1",
+    "url": "/hr-tech-react/static/media/about.50beeabb.png"
+  },
+  {
+    "revision": "35feb28b0d5f0883ab7ad41f3c39a16f",
+    "url": "/hr-tech-react/static/media/android.35feb28b.jpg"
+  },
+  {
+    "revision": "e92f770a92fc71bccce78cc5dca2d8a8",
+    "url": "/hr-tech-react/static/media/app.e92f770a.jpg"
+  },
+  {
+    "revision": "d4b9789cc85f651bf50361ee25fc9c19",
+    "url": "/hr-tech-react/static/media/digital.d4b9789c.jpg"
+  },
+  {
+    "revision": "44d4d29a685f044c94f4ca5008acb30e",
+    "url": "/hr-tech-react/static/media/net.44d4d29a.jpg"
+  },
+  {
+    "revision": "91ddd5fc5465fba753dfa5df7bd631bb",
+    "url": "/hr-tech-react/static/media/software.91ddd5fc.jpg"
+  },
+  {
+    "revision": "443e6daa01efb632b7178ffd35e3ee1f",
+    "url": "/hr-tech-react/static/media/web.443e6daa.png"
+  },
+  {
+    "revision": "512999ef541e05c4c2163d9c69351459",
+    "url": "/hr-tech-react/static/media/web.512999ef.jpg"
+  }
+]);
